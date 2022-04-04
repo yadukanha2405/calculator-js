0b6b5a2f0a46c6b1fca2bce79792f31b035a1484
Kanhaiya Yadav <101487969+yadukanha2405@users.noreply.github.com>
2022-04-05 00:02:10 +0530

    building calculator using javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+import "./index.css";
+
+let display = document.getElementById("display");
+let button = Array.from(document.getElementsByClassName("button"));
+let arr = [];
+
+button.map((button) => {
+  button.addEventListener("click", (e) => {});
+});
