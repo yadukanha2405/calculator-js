7c13a0d28f00b416a2b01c979649e7e0adc8ef92
Kanhaiya Yadav <101487969+yadukanha2405@users.noreply.github.com>
2022-04-07 07:57:47 +0530

    JS logic added

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-import "./index.css";
+import "./styles.css";
 
 let display = document.getElementById("display");
-let button = Array.from(document.getElementsByClassName("button"));
+let buttons = Array.from(document.getElementsByClassName("button"));
 let arr = [];
 
-button.map((button) => {
-  button.addEventListener("click", (e) => {});
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "C":
+        display.innerText = "";
+        arr = [];
+        break;
+
+      case "=":
+        try {
+          display.innerText = eval(arr.join(""));
+        } catch {
+          display.innerText = "Error";
+        }
+        break;
+      case "Back":
+        if (display.innerText) {
+          display.innerText = arr.join("").slice(0, -1);
+          arr.pop();
+        }
+        break;
+      default:
+        display.innerText = e.target.innerText;
+        arr.push(e.target.innerText);
+    }
+  });
 });
